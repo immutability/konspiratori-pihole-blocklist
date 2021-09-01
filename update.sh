4d31bce5fb9e7b2d9adaf954ed1401defa34f5dc
Radoslav Bielik <immutability@bielik.org>
2021-09-01 22:13:40 +0200

    As the pihole adlist format doesn't support wildcard domains or regex, updated the script to add www version of each domain to the processed file

--- update.sh
+++ update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# Update the pihole adlist with domains from konspiratori.sk
+
+# Update the pihole adlist with domains from konspiratori.sk.
+# Remove extra columns from the source CSV, sort domains by name, add "www" version of each domain.
 
 BASEDIR=$(dirname $0)
 curl 'https://konspiratori.sk/static/lists/zoznam.csv' > "${BASEDIR}/konspiratori.source"
-cat "${BASEDIR}/konspiratori.source" | awk -F ',' '{print $1}' | sort > "${BASEDIR}/konspiratori.list"
+cat "${BASEDIR}/konspiratori.source" | awk -F ',' '{print $1}' | sort | sed 's/.*/&\nwww.&/' > "${BASEDIR}/konspiratori.list"
