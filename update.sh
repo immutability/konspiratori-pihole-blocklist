766fd003fcc89516827f8a47796becb68930dbf9
Radoslav Bielik <immutability@bielik.org>
2021-09-01 21:47:25 +0200

    update script updated with an up-to-date source URL

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 # Update the pihole adlist with domains from konspiratori.sk
 
 BASEDIR=$(dirname $0)
-curl 'https://www.konspiratori.sk/assets/downloads/zoznam.txt' > "${BASEDIR}/konspiratori.source"
+curl 'https://konspiratori.sk/static/lists/zoznam.csv' > "${BASEDIR}/konspiratori.source"
 cat "${BASEDIR}/konspiratori.source" | awk -F ',' '{print $1}' | sort > "${BASEDIR}/konspiratori.list"
